7954259423f7d8fef6c7ed251b72400423167413
BrendaGarcia <alejalvarez46@gmail.com>
2017-11-18 15:51:50 -0400

    commit de brenda

---
+++ src/aplicacioncompartida/Brenda.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aplicacioncompartida;
+
+/**
+ *
+ * @author LabB6
+ */
+public class Brenda {
+    public void imprimir(){
+    System.out.println("Hola mundo"); 
+    }
+}
+
