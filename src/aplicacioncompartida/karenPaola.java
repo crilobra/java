b6592dee11b7362454396ddf96dc717340410e30
crilobra <crilobra@gmail.com>
2017-11-18 15:50:23 -0400

    segundo comit

--- src/aplicacioncompartida/karenPaola.java
+++ src/aplicacioncompartida/karenPaola.java
@@ -10,5 +10,15 @@ package aplicacioncompartida;
  * @author LabB8
  */
 public class karenPaola {
+    private String nombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
