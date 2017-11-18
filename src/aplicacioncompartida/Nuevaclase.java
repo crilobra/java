5020a047837820446ddea479d94c26ce9c3ef645
ingridtavi <33790028+ingridtavi@users.noreply.github.com>
2017-11-18 15:33:16 -0400

    4to commit Merge branch 'master' of https://github.com/crilobra/java

--- src/aplicacioncompartida/Nuevaclase.java
+++ src/aplicacioncompartida/Nuevaclase.java
@@ -10,13 +10,27 @@ package aplicacioncompartida;
  * @author LabB6
  */
 public class Nuevaclase {
-    String n1="hola";
+    private String n1="hola";
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+
+    /**
+     * @return the n1
+     */
+    public String getN1() {
+        return n1;
+    }
+
+    /**
+     * @param n1 the n1 to set
+     */
+    public void setN1(String n1) {
+        this.n1 = n1;
     }
     
 }
